edfde5537b10acab30bde5f35e115d1ae92756bf
Lucia <lu.ninez@gmail.com>
2018-10-24 21:36:04 +0200

    Añadida carpeta del proyecto Lombok

---
+++ Ejercicios de practica Lombok/PrimerContacto/SRC/MAIN/JAVA/COM/LBENITEZ/LOMBOK/PrimerContactoApplication.java
@@ -0,0 +1,12 @@
+package com.lbenitez.lombok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerContactoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimerContactoApplication.class, args);
+	}
+}
